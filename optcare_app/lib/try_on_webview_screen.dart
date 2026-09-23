f7320b2eccae12fa31d40e2640003cf1e92bdc53
wahaaah <csavariz29@gmail.com>
2026-09-23 21:55:15 +0800

    Handle WebView camera permissions for Android
    Refactor TryOnWebViewScreen.initState to build a WebViewController with JavaScript and navigation delegates, add debug logging, and import webview_flutter_android. Add Android-specific permission handling via setOnPlatformPermissionRequest to grant requests (enabling camera access for the virtual try-on), and move loadRequest(url). This lets the try-on web page request camera access on Android devices.

--- optcare_app/lib/try_on_webview_screen.dart
+++ optcare_app/lib/try_on_webview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
 
 class TryOnWebViewScreen extends StatefulWidget {
   final int frameId;
@@ -19,56 +20,60 @@ class _TryOnWebViewScreenState
 
   late final WebViewController _controller;
 
-  @override
-  void initState() {
-    super.initState();
+@override
+void initState() {
+  super.initState();
 
-    final url = Uri.parse(
-      'https://gonzalesvisionclinic.vercel.app/',
-    ).replace(
-      queryParameters: {
-        'frameId': widget.frameId.toString(),
-        'mobileTryOn': 'true',
+  final url = Uri.parse(
+    'https://gonzalesvisionclinic.vercel.app/',
+  ).replace(
+    queryParameters: {
+      'frameId': widget.frameId.toString(),
+      'mobileTryOn': 'true',
+    },
+  );
+
+  debugPrint('TRY-ON URL: $url');
+
+  _controller = WebViewController()
+    ..setJavaScriptMode(
+      JavaScriptMode.unrestricted,
+    )
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onPageStarted: (url) {
+          debugPrint(
+            'TRY-ON PAGE STARTED: $url',
+          );
+        },
+        onPageFinished: (url) {
+          debugPrint(
+            'TRY-ON PAGE FINISHED: $url',
+          );
+        },
+        onWebResourceError: (error) {
+          debugPrint(
+            'TRY-ON WEBVIEW ERROR: ${error.description}',
+          );
+        },
+      ),
+    );
+
+  // Allow the web page to request camera access.
+  if (_controller.platform is AndroidWebViewController) {
+    final androidController =
+        _controller.platform as AndroidWebViewController;
+
+    androidController.setOnPlatformPermissionRequest(
+      (request) {
+        debugPrint(
+          '🌐 WebView permission request: ${request.types}',
+        );
+
+        request.grant();
       },
     );
-
-    debugPrint('TRY-ON URL: $url');
-
-    _controller = WebViewController()
-      ..setJavaScriptMode(
-        JavaScriptMode.unrestricted,
-      )
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (url) {
-            debugPrint(
-              'TRY-ON PAGE STARTED: $url',
-            );
-          },
-          onPageFinished: (url) {
-            debugPrint(
-              'TRY-ON PAGE FINISHED: $url',
-            );
-          },
-          onWebResourceError: (error) {
-            debugPrint(
-              'TRY-ON WEBVIEW ERROR: ${error.description}',
-            );
-          },
-        ),
-      )
-      ..loadRequest(url);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Virtual Try-On'),
-      ),
-      body: WebViewWidget(
-        controller: _controller,
-      ),
-    );
-  }
+  _controller.loadRequest(url);
 }
